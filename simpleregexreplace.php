f4be2f537b07f949a2936843e6ffa52dd1c0898b
Greg Gannicott <greg@gannicott.co.uk>
2010-11-19 14:40:01 +0000

    Case 363
    - Fixed bug that resulted in the 'replacement' being displayed incorrectly in the edit form when HTML was involved. As well as performing a striptslashes() against it, I've also used htmlentities().
    - Applied stripslashes() to all areas where database contents are displayed.

--- simpleregexreplace.php
+++ simpleregexreplace.php
@@ -186,19 +186,19 @@ function simple_regex_replace_options_page() {
             // Description
             print '<tr valign="top">';
             print '<th scope="row">Description:</th>';
-            print '<td><input size="50" type="text" name="simple_regex_replace_description" value="'.$current['description'].'" /></td>';
+            print '<td><input size="50" type="text" name="simple_regex_replace_description" value="'.stripcslashes($current['description']).'" /></td>';
             print '</tr>';
 
             // Pattern to Match
             print '<tr valign="top">';
             print '<th scope="row">Pattern to match:</th>';
-            print '<td><input size="50" type="text" name="simple_regex_replace_pattern" value="'.$current['pattern'].'" /></td>';
+            print '<td><input size="50" type="text" name="simple_regex_replace_pattern" value="'.htmlentities(stripslashes($current['pattern'])).'" /></td>';
             print '</tr>';
 
             // Replace With
             print '<tr valign="top">';
             print '<th scope="row">Replace with:</th>';
-            print '<td><input size="50" type="text" name="simple_regex_replace_replace" value="'.$current['replace'].'" /></td>';
+            print '<td><input size="50" type="text" name="simple_regex_replace_replace" value="'.htmlentities(stripslashes($current['replace'])).'" /></td>';
             print '</tr>';
 
          print '</table>';
@@ -238,8 +238,8 @@ function simple_regex_replace_options_page() {
             }
 
             // Generate the HTML for each row:
-            $rows_html .= "<tr ".$class."><td>".$entry['description']."</td>";
-            $rows_html .= "<td>".stripslashes($entry['pattern'])."</td>";
+            $rows_html .= "<tr ".$class."><td>".stripcslashes($entry['description'])."</td>";
+            $rows_html .= "<td>".htmlentities(stripslashes($entry['pattern']))."</td>";
             $rows_html .= "<td>".htmlentities(stripslashes($entry['replace']))."</td>";
             $rows_html .= '<td><a href="'.get_bloginfo( "wpurl").'/wp-admin/options-general.php?page='.array_pop(explode("/",__FILE__)).'&action=update&id='.$entry['id'].'">Edit</a> | <a href="'.get_bloginfo( "wpurl").'/wp-admin/options-general.php?page='.array_pop(explode("/",__FILE__)).'&action=delete&id='.$entry['id'].'">delete</a></td></tr>';
          }
